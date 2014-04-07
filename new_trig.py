cabb5207b293812281175a53700dded970e23ffa
dustinfreeman <dustin.freeman@gmail.com>
2014-04-06 21:58:38 -0400

    initial checkin.

---
+++ new_trig.py
@@ -0,0 +1,53 @@
+# An implementation of the 27 trig functions discussed in The Onion's article:
+# Nation’s Math Teachers Introduce 27 New Trig Functions
+# http://www.theonion.com/articles/nations-math-teachers-introduce-27-new-trig-functi,33804/
+
+# WASHINGTON—Adding to the six basic functions that have for years made up the foundation of trigonometry, the nation’s mathematics teachers reportedly introduced 27 new functions today that high schoolers will be expected to master. “While the core of the trigonometry curriculum has traditionally consisted solely of sine, cosine, tangent, secant, cosecant, and cotangent, henceforth we will be including gasmin, negtan, cosvnx, and two dozen others, such as tosna and cotosna, that our pupils will need to have down pat in order to pass,” Coolidge Senior High School trig teacher Robert Beckman said on behalf of the nation’s math educators, emphasizing that students will be required to have full understanding of tofsin, pomen, cocosine, phyxyx, fotsin, and fostin as they apply to the various properties of equilateral, isosceles, and scalene triangles. “Students will also need to know the corresponding graphs for the functions. For example, drin forms a sort of stepladder going up the X and Y axes, while codrin forms a stepladder going down. I can assure you that all of these are absolutely crucial to understanding basic trigonometry, not to mention a requisite for anyone seeking to graduate and move on to college.” Beckman added that factoring will be cut from the math curriculum entirely because it’s “annoying and too fucking hard sometimes.”
+
+def gasmin(x):
+	pass
+
+def negtan(x):
+	pass
+
+def cosvnx(x):
+	pass
+
+
+def tosna(x):
+	pass
+
+def cotosna(x):
+	pass
+
+
+def tofsin(x):
+	pass
+
+def pomen(x):
+	pass
+
+def cocosine(x):
+	pass
+
+def phyxyx(x):
+	pass
+
+def fotsin(x):
+	pass
+
+def fostin(x):
+	pass
+
+
+def drin(x):
+	# drin forms a sort of stepladder going up the X and Y axes
+	pass
+
+def codrin(x):
+	# codrin forms a stepladder going down
+	pass
+
+
+#Need 14 more functions.
+
