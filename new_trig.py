92101af969133393fe46d2ac2a292f34008b1c5e
dustinfreeman <dustin.freeman@gmail.com>
2014-04-06 22:04:54 -0400

    importing the old trig

--- new_trig.py
+++ new_trig.py
@@ -4,6 +4,8 @@
 
 # WASHINGTON—Adding to the six basic functions that have for years made up the foundation of trigonometry, the nation’s mathematics teachers reportedly introduced 27 new functions today that high schoolers will be expected to master. “While the core of the trigonometry curriculum has traditionally consisted solely of sine, cosine, tangent, secant, cosecant, and cotangent, henceforth we will be including gasmin, negtan, cosvnx, and two dozen others, such as tosna and cotosna, that our pupils will need to have down pat in order to pass,” Coolidge Senior High School trig teacher Robert Beckman said on behalf of the nation’s math educators, emphasizing that students will be required to have full understanding of tofsin, pomen, cocosine, phyxyx, fotsin, and fostin as they apply to the various properties of equilateral, isosceles, and scalene triangles. “Students will also need to know the corresponding graphs for the functions. For example, drin forms a sort of stepladder going up the X and Y axes, while codrin forms a stepladder going down. I can assure you that all of these are absolutely crucial to understanding basic trigonometry, not to mention a requisite for anyone seeking to graduate and move on to college.” Beckman added that factoring will be cut from the math curriculum entirely because it’s “annoying and too fucking hard sometimes.”
 
+from math import *
+
 def gasmin(x):
 	pass
 
